d99fb796e8c5236e2726f4f28f22fd9d2bb1825c
bledsoef <bledsoef@berea.edu>
2023-08-11 00:00:19 -0400

    Finished up the recently listened to section

---
+++ spoti-my/My Library.swift
@@ -0,0 +1,20 @@
+//
+//  My Library.swift
+//  spoti-my
+//
+//  Created by Finn Bledsoe on 8/10/23.
+//
+
+import SwiftUI
+
+struct Your_Library: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Your_Library_Previews: PreviewProvider {
+    static var previews: some View {
+        Your_Library()
+    }
+}
