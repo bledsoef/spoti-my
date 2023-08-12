75120008e9d67401d617870f08eede2cabbe82f6
bledsoef <bledsoef@berea.edu>
2023-08-12 15:41:48 -0400

    Seperated the NowListening view from Home

--- spoti-my/Home.swift
+++ spoti-my/Home.swift
@@ -50,47 +50,7 @@ struct Home: View {
                     JumpBackIn()
 
                 }
-                VStack {
-                    Spacer()
-                    HStack {
-                        Image("SCARING_THE_HOES")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .cornerRadius(3)
-                            .padding(.vertical, 6)
-                            .padding(.horizontal, 4)
-                        VStack {
-                            HStack {
-                                Text("Burfict!")
-                                Spacer()
-                            }
-                            HStack {
-                                Text("JPEGMAFIA, Danny Brown")
-                                    .foregroundColor(.gray)
-                                Spacer()
-                            }
-                            
-                        }
-                        .font(.caption2)
-                        Spacer()
-                        Image(systemName: "hifispeaker")
-                            .scaleEffect(1.2)
-                            .padding(.trailing, 20)
-                        
-                        Image(systemName: "play.fill")
-                            .scaleEffect(1.2)
-                            .padding(.trailing, 10)
-
-                        Spacer()
-                    }
-                    .foregroundColor(.white)
-                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 50)
-                    .background(CustomColors.NowListening)
-                    .clipped()
-                    .cornerRadius(3)
-                    .padding(4)
-                    .shadow(radius: 2)
-                }
+                NowListening()
             }
             .toolbar {
                 ToolbarItemGroup(placement: .navigation) {
