408a3cee09dab4a59c47f6532b2201815fd274dc
bledsoef <bledsoef@berea.edu>
2023-08-12 12:42:41 -0400

    Clicking search bar switches views

--- spoti-my/Search.swift
+++ spoti-my/Search.swift
@@ -8,55 +8,73 @@
 import SwiftUI
 
 struct Search: View {
+    @State var searchBarClicked: Bool = false
     var body: some View {
         NavigationStack {
-            ZStack {
-                CustomColors.HomeBackground.ignoresSafeArea()
-                VStack {
-                    Button(action: {}, label: {
-                        HStack {
-                            Image(systemName: "magnifyingglass")
-                                .scaleEffect(1.3)
-                            Text("What do you want to listen to?")
-                            Spacer()
+            if !searchBarClicked {
+                ZStack {
+                    CustomColors.HomeBackground.ignoresSafeArea()
+                    VStack {
+                        Button(action: toggleSearchBarClicked, label: {
+                            HStack {
+                                Image(systemName: "magnifyingglass")
+                                    .scaleEffect(1.3)
+                                Text("What do you want to listen to?")
+                                Spacer()
+                            }
+                            .foregroundColor(.black)
+                            
+                            
+                            
+                        })
+                        .padding(13)
+                        .background(.white)
+                        .foregroundColor(.black)
+                        .clipped()
+                        .cornerRadius(7)
+                        .padding(.horizontal)
+                        .padding(.top, 5)
+                        Spacer()
                     }
-                    .foregroundColor(.black)
-                        
-
-
-                    })
-                    .padding(13)
-                    .background(.white)
-                    .foregroundColor(.black)
-                    .clipped()
-                    .cornerRadius(7)
-                    .padding(.horizontal)
-                    .padding(.top, 5)
-                    Spacer()
                 }
-            }
-            .toolbar {
-                ToolbarItemGroup(placement: .navigation) {
-                    HStack {
-                        Text("Search")
-                            .foregroundColor(.white)
-                            .font(.system(size: 22, weight: .bold))
+                .toolbar {
+                    ToolbarItemGroup(placement: .navigation) {
+                        HStack {
+                            Text("Search")
+                                .foregroundColor(.white)
+                                .font(.system(size: 22, weight: .bold))
+                        }
+                        
+                    }
+                    ToolbarItemGroup(placement: .navigationBarTrailing) {
+                        HStack {
+                            Image(systemName: "camera")
+                                .padding(4)
+                        }
+                        .foregroundColor(.white)
                     }
                     
                 }
-                ToolbarItemGroup(placement: .navigationBarTrailing) {
-                    HStack {
-                        Image(systemName: "camera")
-                            .padding(4)
-                    }
-                    .foregroundColor(.white)
+            }
+            else {
+                ZStack {
+                    CustomColors.HomeBackground.ignoresSafeArea()
                 }
-                
+                .toolbar {
+                    ToolbarItemGroup(placement: .navigationBarTrailing) {
+                        HStack {
+                            Text("Cancel")
+                                .foregroundColor(.white)
+                                .font(.system(size: 15, weight: .bold))
+                        }
+                        
+                    }
+                }
             }
         }
     }
-    func getRexStatus() {
-        
+    func toggleSearchBarClicked() {
+        searchBarClicked.toggle()
     }
 }
 
