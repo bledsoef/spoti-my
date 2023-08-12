999d430de63972c8b68ab5b88c0cfb4356156080
bledsoef <bledsoef@berea.edu>
2023-08-12 13:38:11 -0400

    added now listening bar, needs a little work

--- spoti-my/Home.swift
+++ spoti-my/Home.swift
@@ -32,6 +32,7 @@ struct Home: View {
         NavigationStack {
             
             ZStack {
+
                 CustomColors.HomeBackground.ignoresSafeArea()
                 VStack {
                     FilterButtons()
@@ -47,8 +48,48 @@ struct Home: View {
                     }
                     IndieAlbums()
                     JumpBackIn()
-                        
+
+                }
+                VStack {
                     Spacer()
+                    HStack {
+                        Image("SCARING_THE_HOES")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(3)
+                            .padding(.vertical, 6)
+                            .padding(.horizontal, 4)
+                        VStack {
+                            HStack {
+                                Text("Burfict!")
+                                Spacer()
+                            }
+                            HStack {
+                                Text("JPEGMAFIA, Danny Brown")
+                                    .foregroundColor(.gray)
+                                Spacer()
+                            }
+                            
+                        }
+                        .font(.caption2)
+                        Spacer()
+                        Image(systemName: "hifispeaker")
+                            .scaleEffect(1.2)
+                            .padding(.trailing, 20)
+                        
+                        Image(systemName: "play.fill")
+                            .scaleEffect(1.2)
+                            .padding(.trailing, 10)
+
+                        Spacer()
+                    }
+                    .foregroundColor(.white)
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 50)
+                    .background(CustomColors.NowListening)
+                    .clipped()
+                    .cornerRadius(3)
+                    .padding(4)
+                    .shadow(radius: 2)
                 }
             }
             .toolbar {
